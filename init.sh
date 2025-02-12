5e8f0dec852042312b493ba37f9f2fb75e8842f8
Max Paulus <m.paulus@uva.nl>
2025-02-12 09:09:10 +0100

    added auth

--- init.sh
+++ init.sh
@@ -4,7 +4,7 @@
 nginx_conf="/etc/nginx/conf.d/ssl_main.conf"
 
 # Let Nginx serve the docker container running on port 5000 instead of a static web page
-sed -i 's|root /var/www/html;|location \/ {\n    \tproxy_pass http:\/\/localhost:5000;\n    \tproxy_set_header Host $host;\n    \tproxy_set_header X-Real-IP $remote_addr;\n    }|' "$nginx_conf"
+sed -i 's|root /var/www/html;|location \/ {\n    \tproxy_pass http:\/\/localhost:5000;\n    \tproxy_set_header Host $host;\n    \tproxy_set_header X-Real-IP $remote_addr;\n    \terror_page 401 = @custom_401;\n    \tauth_request /validate;\n    \tauth_request_set $username $upstream_http_username;\n    \tproxy_set_header REMOTE_USER $username;\n    }|' "$nginx_conf"
 sed -i 's|index index.html index.htm;||' "$nginx_conf"
 
 systemctl restart nginx.service
